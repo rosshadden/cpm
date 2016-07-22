64dc5b85c06b66cd8bdae2e14efcaff9b686dec9
Ross Hadden <rosshadden@gmail.com>
2016-07-22 15:52:01 -0400

    Started making the bin.

--- bin/cpm.js
+++ bin/cpm.js
@@ -1,1 +1,17 @@
-console.log('TODO');
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+const toml = require('toml');
+
+let cwd = path.resolve('.');
+let cosmicPath = path.resolve(cwd, 'cosmic.toml');
+
+yargs
+	.usage('$0 <cmd> [args]')
+	.command('run <action>', 'Run an action defined in the package file.', {}, () => {
+		fs.readFile(cosmicPath, 'utf-8', (err, data) => {
+			console.log(data);
+		});
+	})
+	.help('help')
+	.argv;
